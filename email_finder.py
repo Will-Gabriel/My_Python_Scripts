e527f09e0c4050c170bd73225743502d7d768c8c
Wilian <103289184+Will-Gabriel@users.noreply.github.com>
2024-10-03 15:53:49 -0300

    Update email_finder.py

--- email_finder.py
+++ email_finder.py
@@ -10,7 +10,7 @@ EMAILS = []
 
 
 def request(url):
-    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"}
     try:
         response = requests.get(url, headers=header)
         return response.text
